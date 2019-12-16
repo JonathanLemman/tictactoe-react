c6420c0cd47096ab9a79ea90123c7aadc6d70ad6
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-12-16 14:27:33 -0600

    added props from parents passed to child and set state

--- src/index.js
+++ src/index.js
@@ -3,20 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-
 class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button
+                className="square"
+                onClick={() => this.setState({value: 'X'})}
+            >
+                {this.state.value}
             </button>
         );
     }
 }
 
+
+
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square />;
+        return <Square value={i} />;
     }
 
     render() {
@@ -44,6 +55,8 @@ class Board extends React.Component {
         );
     }
 }
+
+
 
 class Game extends React.Component {
     render() {
